e29508d599d12cbebacf66fe8905144192929c78
tai0312 <145961519+tai0312@users.noreply.github.com>
2023-12-03 01:33:55 +0900

    canvasに画像表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState,useRef } from "react";
-/*
+
 const pieceSize = 100;
-let canField = document.getElementById('can');
-let canText = canField.getContext('2d');
 let widthCnt = 0;
 let lengthCnt = 0;
 
@@ -12,19 +10,20 @@ class Piece {
         this.Outline = outline;
         this.X = x;
         this.Y = y;
-        this.OriginalWidth = Math.round(x/pieceSize);
-        this.OriginalLength = Math.round(y/pieceSize);
+        this.OriginalW = Math.round(x/pieceSize);
+        this.OriginalL = Math.round(y/pieceSize);
     }
     Draw(){
         canText.drawImage(this.Image,this.X,this.Y);
         canText.drawImage(this.Outline,this.X,this.Y);
     }
 }
-*/
+
 
 async function fetchData(){
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const image = await response.json();
+    console.log(image);
     return image.message;
 }
 
@@ -33,9 +32,11 @@ async function fetchData(){
 export default function App(){
     const [content,setContent] = useState({});
     const canRef = useRef(null);
+    const imgRef = useRef(null);
     useEffect (() => {
         (async () =>{ 
             const newContent = await fetchData();
+            console.log(newContent);
             setContent(newContent);
         })();
     },[]);
@@ -43,16 +44,31 @@ export default function App(){
         <>
         <form onSubmit={(event)=>{
             event.preventDefault();
-            const canField = canRef.current.getContext('2d');
-            canField.drawImage(content,0,0)
+            const canField = canRef.current;
+            const canContent = canField.getContext('2d');
+            const imgContent = imgRef.current;
+
+            canField.width = imgContent.width*2;
+            canField.height = imgContent.height*2;
+
+            let imgWidth = 600;
+            let imgHeight=imgWidth*imgContent.height/imgContent.width;
+            
+            const myImg = new Image();
+            myImg.src = imgContent.src;
+            canContent.drawImage(myImg,0,0,imgWidth,imgHeight);
         }}>
             <p>
                 <button type="submit" id="shuffle">シャッフル</button>
             </p>
             <canvas ref={canRef}></canvas>
+            <div className="bufferImg">
+                <img ref={imgRef} src={content}/>
+            </div>
+            
             
         </form>
-        <img src={content}/>
+        
         
         </>
     );
