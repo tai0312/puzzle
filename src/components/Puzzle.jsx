5e181156df04ed1fdf850c3e1785d74ab6d70b89
tai03 <tdmius03@gmail.com>
2024-01-08 23:07:28 +0900

    パズル作成

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -40,6 +40,7 @@ export default function Puzzle({ imageUrl, imageSize }){
                 //setPieces(newPieces.sort(() => Math.random() - 0.5));
                 setPieces(newPieces);
                 console.log(image.width);
+                console.log(newPieces);
             }
         })();
     }, [image]);
