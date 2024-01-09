801f4e13edc7326b8523e184989956ebc90e950b
tai03 <tdmius03@gmail.com>
2024-01-10 01:50:51 +0900

    PuzzlePieceコンポーネント削除実験

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -202,23 +202,24 @@ export default function Puzzle({ imageUrl }){
 
     return (
         <Stage width={puzzleSize.cols * PIECE_SIZE*2} height={puzzleSize.rows * PIECE_SIZE*1.5}>
-            <Layer ref={layerRef}/*onMouseDown={mouseMove}*//>
-            <Rect
-                stroke='black'
-                strokeWidth={3}
-                x={0}
-                y={0}
-                width={puzzleSize.cols * PIECE_SIZE * 2}
-                height={puzzleSize.rows * PIECE_SIZE * 1.5}
-            />
-            <Rect
-                stroke='black'
-                strokeWidth={3}
-                x={7}
-                y={7}
-                width={puzzleSize.cols * PIECE_SIZE + 5}
-                height={puzzleSize.rows * PIECE_SIZE + 5}
-            />
+            <Layer ref={layerRef}/*onMouseDown={mouseMove}*/>
+                <Rect
+                    stroke='black'
+                    strokeWidth={3}
+                    x={0}
+                    y={0}
+                    width={puzzleSize.cols * PIECE_SIZE * 2}
+                    height={puzzleSize.rows * PIECE_SIZE * 1.5}
+                />
+                <Rect
+                    stroke='black'
+                    strokeWidth={3}
+                    x={7}
+                    y={7}
+                    width={puzzleSize.cols * PIECE_SIZE + 5}
+                    height={puzzleSize.rows * PIECE_SIZE + 5}
+                />
+            </Layer>
         </Stage>
     );
 }
