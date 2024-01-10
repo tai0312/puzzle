91a1a30b887d21e01302748a865ec9e3e5ce8a8c
tai <tdmius03@gmaim.com>
2024-01-10 10:59:35 +0900

    header追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 //import React from 'react';
-import { makeStyles } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
