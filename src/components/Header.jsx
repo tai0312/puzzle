3c550ea1afa0c36b3a017b175e6214a9d7386496
tai <tdmius03@gmaim.com>
2024-01-10 11:16:51 +0900

    header追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,27 @@
 //import React from 'react';
-import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/material/Menu';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+const DivRoot = styled('div')(() => ({
+  flexGrow: 1,
 }));
 
+/*const DialogTypography = styled(Typography)(() => ({
+  flexGrow: 1,
+}));*/
+
 export default function ButtonAppBar() {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
+    <DivRoot>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6">
             News
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    </div>
+    </DivRoot>
   );
 }
